8f747996e7bf4f6ab08dcf52f7b2112045581cb9
sherylynn <352281674@qq.com>
2018-01-03 16:12:18 +0800

    更新

--- secrets.sh
+++ secrets.sh
@@ -8,3 +8,8 @@ drone secret add \
     --name=ssh_key \
     --value=@$HOME/.ssh/id_rsa\
     --image=appleboy/drone-ssh
+
+
+#如果git服务器不是ip地址 似乎不能直接git clone下来
+#而用外网ip,git clone起来非常慢
+#如果用内网ip,git clone 不下来
