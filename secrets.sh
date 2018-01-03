7f9cc26fa82daed6e37775b58c07aa46719cd086
sherylynn <352281674@qq.com>
2018-01-03 15:51:42 +0800

    自动添加secret

---
+++ secrets.sh
@@ -0,0 +1,10 @@
+#drone secrets add \
+#    --repository=p/game \
+#    --name=ssh_key \
+#    --value="$(sudo cat ~/.ssh/id_rsa)" \
+#    --image=appleboy/drone-ssh
+drone secret add \
+    --repository=p/game \
+    --name=ssh_key \
+    --value=@"$($HOME)"/.ssh/id_rsa\
+    --image=appleboy/drone-ssh
