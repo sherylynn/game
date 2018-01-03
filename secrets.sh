d80882c130eb041c7bee644108d0ea0b903ba525
sherylynn <352281674@qq.com>
2018-01-03 16:40:48 +0800

    修复

--- secrets.sh
+++ secrets.sh
@@ -1,8 +1,8 @@
-#drone secrets add \
-#    --repository=p/game \
-#    --name=ssh_key \
-#    --value="$(sudo cat ~/.ssh/id_rsa)" \
-#    --image=appleboy/drone-ssh
+drone secret add \
+    --repository=p/game \
+    --name=ssh_key \
+    --value="$(sudo cat ${HOME}/.ssh/id_rsa)" \
+    --image=appleboy/drone-ssh
 #drone secret add \
 #    --repository=p/game \
 #    --name=ssh_key \
@@ -13,3 +13,11 @@
 #如果git服务器不是ip地址 似乎不能直接git clone下来
 #而用外网ip,git clone起来非常慢
 #如果用内网ip,git clone 不下来
+docker run --rm \
+  -e PLUGIN_HOST=tx.18e.pub \
+  -e PLUGIN_USERNAME=ubuntu \
+  -e PLUGIN_KEY="$(cat ${HOME}/.ssh/id_rsa)" \
+  -e PLUGIN_SCRIPT=whoami \
+  -v $(pwd):$(pwd) \
+  -w $(pwd) \
+  appleboy/drone-ssh
