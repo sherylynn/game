a6f043a50a42c00d8079778ad0ff2708f712a6c1
sherylynn <352281674@qq.com>
2018-01-03 16:18:37 +0800

    迷茫

--- secrets.sh
+++ secrets.sh
@@ -3,12 +3,12 @@
 #    --name=ssh_key \
 #    --value="$(sudo cat ~/.ssh/id_rsa)" \
 #    --image=appleboy/drone-ssh
-drone secret add \
-    --repository=p/game \
-    --name=ssh_key \
-    --value=@$HOME/.ssh/id_rsa\
-    --image=appleboy/drone-ssh
-
+#drone secret add \
+#    --repository=p/game \
+#    --name=ssh_key \
+#    --value=@$HOME/.ssh/id_rsa\
+#    --image=appleboy/drone-ssh
+# 直接指定@似乎不生效？
 
 #如果git服务器不是ip地址 似乎不能直接git clone下来
 #而用外网ip,git clone起来非常慢
