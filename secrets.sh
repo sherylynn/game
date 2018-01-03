42ce89360651c6d1fd68dc2a6c0630bde2998b13
sherylynn <352281674@qq.com>
2018-01-03 16:25:28 +0800

    again

--- secrets.sh
+++ secrets.sh
@@ -9,7 +9,7 @@
 #    --value=@$HOME/.ssh/id_rsa\
 #    --image=appleboy/drone-ssh
 # 直接指定@似乎不生效？
-#again
+#again again
 #如果git服务器不是ip地址 似乎不能直接git clone下来
 #而用外网ip,git clone起来非常慢
 #如果用内网ip,git clone 不下来
